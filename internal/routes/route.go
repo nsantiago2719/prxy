73171fdebb0351445fc0f104aed9d1f8c7c5db16
Norman Santiago <nsantiago@ethernalabs.com>
2025-09-24 21:50:54 +0800

    feat: returns an error with message when there are errors in the proxy

--- internal/routes/route.go
+++ internal/routes/route.go
@@ -2,6 +2,8 @@
 package routes
 
 import (
+	"encoding/json"
+	"log/slog"
 	"net/http"
 
 	"github.com/nsantiago2719/prxy/internal/handlers"
@@ -9,12 +11,24 @@ import (
 
 type handlerFunc func(http.ResponseWriter, *http.Request) error
 
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
 func makeHandler(f handlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Call the handler function
 		if err := f(w, r); err != nil {
-			// Log the error
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			// Log the error and return the error
+			err := Error{
+				Code:    http.StatusInternalServerError,
+				Message: err.Error(),
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(err)
+			slog.Error(err.Message)
 		}
 	}
 }
