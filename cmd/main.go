1b42fcb07c72fedf42299e1709bde7c63c0c4934
Norman Santiago <nsantiago@ethernalabs.com>
2025-10-01 18:06:43 +0800

    feat: initial configuration feature

--- cmd/main.go
+++ cmd/main.go
@@ -8,17 +8,21 @@ import (
 )
 
 func main() {
-	router := routes.NewRoutes()
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: router,
-		// Good practice to set timeouts to avoid Slowloris attacks.
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-
+	server := configureServer()
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func configureServer() http.Server {
+	handlers := routes.NewRoutes()
+
+	return http.Server{
+		Addr:    ":8080",
+		Handler: handlers,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
 }
